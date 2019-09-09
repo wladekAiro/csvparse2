1f64079e4bae5483c4e2a90a1833910951a9a1de
Wladek <wladek.airo@gmail.com>
2019-09-09 14:58:06 +0300

    refactor

--- src/CsvProcessor.java
+++ src/CsvProcessor.java
@@ -1,6 +1,68 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 class CsvProcessor {
+
+    static String processFile(String del, String filePath, List<String> erroredLines, List<Student> students) {
+
+        String line;
+        String fileHeader = null;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+            int lineNumber = 1;
+
+            while ((line = br.readLine()) != null) {
+
+                if (!line.contains(del)) {
+                    del = getDelimiter();
+                }
+
+                if (lineNumber > 1) {
+                    // use del as separator
+                    String[] fileLine = line.split(del);
+                    validateFileLine(lineNumber, fileLine, erroredLines, students);
+                }else {
+                    fileHeader = line;
+                }
+
+                lineNumber++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileHeader;
+    }
+
+    private static void validateFileLine(int lineNumber, String[] fileLine, List<String> erroredLines, List<Student> students) {
+
+        SimpleDateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+
+            String idStrng = fileLine[0];
+            String firstName = fileLine[1];
+            String lastName = fileLine[2];
+            String grade = fileLine[3];
+            String regDate = fileLine[4];
+
+            Date regiDate = dateFormater.parse(regDate);
+
+            Student student = new Student(Integer.parseInt(idStrng), firstName, lastName, grade, regiDate);
+            students.add(student);
+
+        } catch (Exception e) {
+            String error = " Error file line " + lineNumber + " : " + e.getMessage();
+            erroredLines.add(error);
+        }
+    }
 
     static String getDelimiter(){
         System.out.println(" Please provide your csv file delimiter i.e either ',' or ';' ");
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,9 +14,8 @@ public class Main {
         String del = getDelimiter();
         List<String> erroredLines = new ArrayList<>();
         List<Student> students = new ArrayList<>();
-        String fileHeader = null;
 
-        fileHeader = processFile(del,filePath,erroredLines,students,fileHeader);
+        String fileHeader = CsvProcessor.processFile(del,filePath,erroredLines,students);
 
         if (!students.isEmpty()){
             System.out.println(" STUDENTS FOUND =  " + students.size());
@@ -66,62 +60,6 @@ public class Main {
         } while (!validDel);
 
         return del;
-    }
-
-    private static String processFile(String del, String filePath, List<String> erroredLines, List<Student> students,
-                                      String fileHeader) {
-
-        String line;
-
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
-
-            int lineNumber = 1;
-
-            while ((line = br.readLine()) != null) {
-
-                if (!line.contains(del)) {
-                    del = getDelimiter();
-                }
-
-                if (lineNumber > 1) {
-                    // use del as separator
-                    String[] fileLine = line.split(del);
-                    validateFileLine(lineNumber, fileLine, erroredLines, students);
-                }else {
-                    fileHeader = line;
-                }
-
-                lineNumber++;
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return fileHeader;
-    }
-
-    private static void validateFileLine(int lineNumber, String[] fileLine, List<String> erroredLines, List<Student> students) {
-
-        SimpleDateFormat dateFormater = new SimpleDateFormat("yyyy-MM-dd");
-
-        try {
-
-            String idStrng = fileLine[0];
-            String firstName = fileLine[1];
-            String lastName = fileLine[2];
-            String grade = fileLine[3];
-            String regDate = fileLine[4];
-
-            Date regiDate = dateFormater.parse(regDate);
-
-            Student student = new Student(Integer.parseInt(idStrng), firstName, lastName, grade, regiDate);
-            students.add(student);
-
-        } catch (Exception e) {
-            String error = " Error file line " + lineNumber + " : " + e.getMessage();
-            erroredLines.add(error);
-        }
     }
 
 }
